c3e6ba6a5aeeb632f1353850cc2b6140ddc2c91a
anna <anna@example.com>
2021-06-08 14:09:46 +0300

    first commit

---
+++ components/Logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <a href="tel:+79787770718" class="contacts"><i class="bi bi-telephone"></i>+7 978 777 07 18</a>
+</template>
+
+<script>
+export default {
+  name: "Tel"
+}
+</script>
+
+<style scoped>
+
+</style>
